2f13afb955fee89de43dabf1383b60bfda1ea711
Leo <90lteixeira@gmail.com>
2021-05-03 02:08:31 -0300

    modulo 1

--- curso/modulo1/main2.js
+++ curso/modulo1/main2.js
@@ -5,28 +5,29 @@ class Fila {
 	    this.jobs = [];
 	}
 
-	get getJobs() {
-		this.worker(this._getJobs());
+    get getJobs() {
+ 		this._getJobs();
 	}
 
-	get removeAllJobs() {
-		this.worker(this._removeAllJobs());
+    get removeAllJobs() {
+ 		this._removeAllJobs();
 	}
 
  	addJob = function (job) {
-		this.worker(this._addJob(job));
-	}   
+ 		this._addJob(job); 
+	}
 
 	removeJob = function (id) {
-		this.worker(this._removeJob(id));
+ 		this._removeJob(id);
 	}   
 
 	_getJobs = function () {
-		return this.jobs; 
+		this.processaFila(this.jobs);
 	};
 	
 	_addJob = function (job) {
- 		return this.jobs.push(job); 
+ 		this.jobs.push(job); 
+ 		this.processaFila(job);
 	};
 
 	_removeJob = function (id) {
@@ -40,19 +41,28 @@ class Fila {
 			 	jobs.splice(key, 1); 
 			 	return;
 			}
-		}); 
-
-		return this.jobs;
+		});  
+		this.processaFila();
 	};
 
 	_removeAllJobs = function () {
-	 	return this.jobs = []; 
+  		this.jobs = [];
+  		this.processaFila();
 	};
+
+	processaFila(job) {
+		this.worker(job).then(() => {
+			this.jobs.shift();
+			if (this.jobs.lenght > 0) {
+				this.processaFila();
+			}
+		});
+	}
 }
  
 function myWorker(_job) {
 	return new Promise(resolve => { 
-		setTimeout(() => {
+		setTimeout(() => { 
 			console.log(_job); 
 			resolve();
 		}, 1000);
@@ -74,17 +84,8 @@ const fila = new Fila(myWorker);
 
 fila.addJob(criarJob(
 	'Cevadis im ampola pa arma uma pindureta. Viva Forevis aptent taciti sociosqu ad litora torquent. Mé faiz elementum girarzis, nisi eros vermeio. Paisis, filhis, espiritis santis. '
-));
-fila.addJob(criarJob(
-	'Aenean aliquam molestie leo, vitae iaculis nisl. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Sapien in monti palavris qui num significa nadis i pareci latim. Manduma pindureta quium dia nois paga. '
-));
-fila.addJob(criarJob(
-	'Delegadis gente finis, bibendum egestas augue arcu ut est. Quem num gosta di mé, boa gentis num é. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. '
 )); 
 
-fila.getJobs; 
- 
-setTimeout(() => {
-	fila.removeJob(fila._getJobs()[1].id);  
-	fila.removeAllJobs; 
-}, 2000);
+fila.addJob(criarJob(
+	'Aenean aliquam molestie leo, vitae iaculis nisl. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Sapien in monti palavris qui num significa nadis i pareci latim. Manduma pindureta quium dia nois paga. '
+)); 
